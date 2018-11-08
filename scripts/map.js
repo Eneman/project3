06b4ac9e9aabc1c482d2e292eb2b9505552e38e7
Eneman <hendrix.bcz@gmail.com>
2018-11-08 17:18:47 +0100

    separation des scripts

---
+++ scripts/map.js
@@ -0,0 +1,12 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([5.37, 43.29]),
+        zoom: 12
+    })
+});
