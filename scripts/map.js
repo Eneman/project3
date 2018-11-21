2bb215a1419d516d79c4f6563cba7b9bee4005f0
Eneman <hendrix.bcz@gmail.com>
2018-11-22 00:22:57 +0100

    updated form revised map management + events on markers

--- scripts/map.js
+++ scripts/map.js
@@ -2,11 +2,11 @@
 var stations;
 var stationsMarkers = L.layerGroup();
 var map = L.map('map').setView([43.29, 5.37], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(map);
 
 getStationsJSON();
 var update = setInterval(() =>{getStationsJSON();}, 10000);
 
-L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(map);
 
 
 
@@ -32,7 +32,6 @@ function getStationsJSON ()
     function (data, textStatus, jqXHR)
     {
         stations = data;
-
         if (stationsMarkers.getLayers().length == 0)
         {
             addMarkers();
@@ -48,8 +47,26 @@ function addMarkers ()
         temp.id = i;
         temp.addTo(stationsMarkers);
     }
+    console.log(stationsMarkers);
     stationsMarkers.addTo(map);
+    stationsMarkers.eachLayer(marker =>
+    {
+        marker.on("click", e =>
+        {
+            writeForm(e.target.id);
+        });
+    });
     
 }
 
-L.
+
+function writeForm(id)
+{
+    document.querySelector("#pName span").textContent = stations[id].name;
+    document.querySelector("#pStatus span").textContent = stations[id].status;
+    document.querySelector("#pBikeStands span").textContent = stations[id].bike_stands;
+    document.querySelector("#pBikes span").textContent = stations[id].available_bikes;
+    document.querySelector("#pEmptyStands span").textContent = stations[id].available_bike_stands;
+    document.querySelector("#pBikeReserved span").textContent = "WIP";
+}
+
