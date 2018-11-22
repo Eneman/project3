8ef7622e5888eb5b34bdaa79c0de06023d74c557
Eneman <hendrix.bcz@gmail.com>
2018-11-22 16:11:46 +0100

    timer functions and reservation

--- scripts/map.js
+++ scripts/map.js
@@ -1,17 +1,28 @@
 
-var stations;
-var stationsMarkers = L.layerGroup();
-var map = L.map('map').setView([43.29, 5.37], 13);
+var stations;                                       // Array of Station Object
+var stationsMarkers = L.layerGroup();               // LayerGroup for all markers(stations)
+var map = L.map('map').setView([43.29, 5.37], 13);  // Init map in map div and set starting coordinates and zoom
+var currentStation;                                 // Id of the last station wrote to form
+var firstName, lastName;
+var timer = 0;
+var bikeReserved = false;
+var btnReservation = document.querySelector("#reservation button");
+
+// Add Graphical Layer to map
 L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(map);
+
+/*----------------------------------------------
+  Get Data a first time, then update every 10sec
+------------------------------------------------*/
 
 getStationsJSON();
 var update = setInterval(() =>{getStationsJSON();}, 10000);
 
+/*------------------------------------------------------------------------------------------
+  Ajout d'une methode a l'objet LayerGroup pour recuperer un marker avec l'id correspondante
+--------------------------------------------------------------------------------------------*/
 
-
-
-// Ajout d'une methode a l'objet LayerGroup pour recuperer un marker avec l'id correspondante
-L.LayerGroup.include
+/* L.LayerGroup.include
 ({
     getMarkers: function(id)
     {
@@ -23,28 +34,38 @@ L.LayerGroup.include
             }
         }
     }
-});
+}); */
+
+/*----------------------------------------
+  Retrieve Data from JCDECAUX OPENDATA API
+------------------------------------------*/
 
 function getStationsJSON ()
 {
-    console.log("requesting data")
     $.getJSON("https://api.jcdecaux.com/vls/v1/stations?contract=Marseille&apiKey=e96ee35df2bc66462b55acf30a90deb607d4ed4b",
     function (data, textStatus, jqXHR)
     {
         stations = data;
+
+        //If no markers already exist, create them and catch info from sessionStorage
         if (stationsMarkers.getLayers().length == 0)
         {
             addMarkers();
+            readSessionStorage();
         }
     });
 }
+
+/*------------------------------------------------------------------------------
+  Create LayerGroup for markers(stations), add event to them and add them to map
+--------------------------------------------------------------------------------*/
 
 function addMarkers ()
 {
     for (var i = 0; i < stations.length; i++)
     {
         let temp = L.marker([stations[i].position.lat, stations[i].position.lng]);
-        temp.id = i;
+        temp.id = i; //Give an id to marker matching the corresponding station in stations Array
         temp.addTo(stationsMarkers);
     }
     console.log(stationsMarkers);
@@ -59,9 +80,15 @@ function addMarkers ()
     
 }
 
+/*-------------------------
+  Add Station infos to form
+---------------------------*/
 
 function writeForm(id)
 {
+    currentStation = id;
+    
+    
     document.querySelector("#pName span").textContent = stations[id].name;
     document.querySelector("#pStatus span").textContent = stations[id].status;
     document.querySelector("#pBikeStands span").textContent = stations[id].bike_stands;
@@ -70,3 +97,55 @@ function writeForm(id)
     document.querySelector("#pBikeReserved span").textContent = "WIP";
 }
 
+function readSessionStorage()
+{
+    
+    firstName = sessionStorage.getItem("firstName");
+    lastName = sessionStorage.getItem("lastName");
+    document.querySelector("#reservation").firstNameInput.value = firstName;
+    document.querySelector("#reservation").lastNameInput.value = lastName;
+
+    if (sessionStorage.getItem("currentStation") != undefined)
+    {
+        currentStation = sessionStorage.getItem("currentStation");
+        timer = parseInt(sessionStorage.getItem("timer"));
+        document.querySelector("#pBikeReserved span").textContent = "OUI";
+        setInterval(timerUpdate(), 1000);
+        bikeReserved = true;
+    }
+
+}
+
+
+function timerUpdate()
+{
+    if (timer > 0)
+    {
+
+        timer--;
+        document.querySelector("#spanTimer").textContent = timer;
+        sessionStorage.setItem("timer", timer);
+    }
+    else
+    {
+        sessionStorage.removeItem("timer");
+        sessionStorage.removeItem("currentStation");
+        document.querySelector("#spanTimerLoc").textContent = "";
+        document.querySelector("#spanTimer").textContent = "";
+    }
+}
+
+btnReservation.addEventListener("click", (e) =>
+{
+    e.preventDefault();
+    if (bikeReserved == false)
+    {
+        sessionStorage.setItem("firstName", document.querySelector("#reservation").firstNameInput.value);
+        sessionStorage.setItem("lastName", document.querySelector("#reservation").lastNameInput.value);
+        sessionStorage.setItem("currentStation", currentStation);
+        timer = 1200;
+        setInterval(timerUpdate(), 1000);
+        bikeReserved = true;
+        document.querySelector("#spanTimerLoc").textContent = stations[currentStation].name;
+    }
+});
